12f7a8773d0e31ed96285964444ce18e5846afb1
Bauka <e444er@gmail.com>
2022-10-19 17:34:48 +0600

    build commit

--- app/src/main/java/com/e444er/multimodule/MainActivity.kt
+++ app/src/main/java/com/e444er/multimodule/MainActivity.kt
@@ -2,10 +2,20 @@ package com.e444er.multimodule
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import com.e444er.multimodule.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding : ActivityMainBinding
+    get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        window.statusBarColor = ContextCompat.getColor(this, R.color.white)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
---
+++ app/src/main/java/com/e444er/multimodule/NewsApp.kt
@@ -0,0 +1,7 @@
+package com.e444er.multimodule
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class NewsApp: Application()
